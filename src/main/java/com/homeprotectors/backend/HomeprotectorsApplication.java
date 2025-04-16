cca9a7dbcfdbc028fea5eb1126be256ace198e33
shayjung <sosat4425@gmail.com>
2025-04-16 20:00:18 +0900

    🔧chore: initial commit

---
+++ src/main/java/com/homeprotectors/backend/HomeprotectorsApplication.java
@@ -0,0 +1,13 @@
+package com.homeprotectors.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeprotectorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeprotectorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeprotectors/backend/HomeprotectorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeprotectors.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeprotectorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
